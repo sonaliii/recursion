2826f1dbc15acc552b883c1de220058892136911
Sonali <sona8242@gmail.com>
2013-12-17 13:21:05 -0800

    Started recursive function with classList and childNodes- tests failing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,29 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+
+    var node = document.body;
+    var result = [];
+
+    if (node.classList.contains(className)) {
+        result.push(node);
+    }
+
+    for (var i = 1; i < node.childNodes.length; i++) {
+        var children = "";
+        var child = node.childNodes[i];
+        if (child.classList.contains(className)) {
+            children += child;
+            result.push(children);
+        }
+        var nodeChild = node.childNodes[i];
+        getElementsByClassName(className)
+    }
+
+    if (nodeChild.classList.contains(className)) {
+        nodeChild.appendTo(result);
+    }
+
+    return result;
 };
+
